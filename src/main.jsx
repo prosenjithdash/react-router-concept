1b2045ffd1ea3e29f55ffb135be6330b5e67a4f8
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-21 18:47:08 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About Section!</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact Section</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
